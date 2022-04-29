0d16158b63353aa1b9db67ec836fd3d95f878f45
yonatan46 <yonatankefyalew65@gmail.com>
2022-04-29 04:40:58 -0700

    msg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
+#include "main.h"
 
-/*
- *
+/**
  *main - Print the name of the program
- *
  *@argc: Count arguments
- *
  *@argv: Arguments
- *
  * Return: Always 0 (Success)
- *
  */
 
-int main (int argc,char **argv)
+int main (int argc, char **argv)
 {
 	printf("%s\n",argv[0]);
 
