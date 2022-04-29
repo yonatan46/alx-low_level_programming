adb07edc57777a2f3c220111fa9116c5ac294e65
yonatan46 <yonatankefyalew65@gmail.com>
2022-04-29 04:55:14 -0700

    msg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**	
- *main - print the name of the program
- *@argc: Count arguments
- *@argv: Arguments
+ * main - print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char **argv)
