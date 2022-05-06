e9884478cd227ae21e8d31c8c35f5b0e3404d1cf
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-06 01:12:33 -0400

    msg

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+/**
+*malloc_checked - function for allocating mem
+*@b: amount to allocate with
+*Return: returns void pointer
+*
+*/
+
+void *malloc_checked(unsigned int b)
+{
+    void *ptr;
+    ptr = malloc(b);
+    if(ptr == NULL)
+    {
+        exit(98);
+    }
+    return(ptr);
+}
