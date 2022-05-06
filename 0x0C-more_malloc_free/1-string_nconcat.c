044287132954e12516da275593de37b467e25eb5
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-06 11:20:03 -0400

    msg

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,34 +6,35 @@
 /**
 *string_nconcat - concatinate 2 string till n size
 *@s1: destination to cat
-*@s2: source to cat 
-*@n:size to cat 
+*@s2: source to cat
+*@n:size to cat
 *Return: pointer to char
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *concat;
-    int x = 0;
-    int y = 0;
+char *concat;
+int x = 0;
+int y = 0;
 
-    concat = malloc(sizeof(s2) * n);
-    if (p == NULL)
-	    return (0);
+concat = malloc(sizeof(s2) * n);
+if (p == NULL)
+{
+return (0);
+}
+while (s1[x])
+{
+concat[x] = s1[x];
+x++;
+}
 
-    while (s1[x])
-    {
-        concat[x] = s1[x];
-        x++;
-    }
+while (y < n)
+{
+concat[x] = s2[y];
+x++;
+y++;
+}
+concat[x] = '\0';
 
-    while (y < n)
-    {
-        concat[x] = s2[y];
-        x++;
-        y++;
-    }
-    concat[x] = '\0';
-
-    return (concat);
+return (concat);
 }
