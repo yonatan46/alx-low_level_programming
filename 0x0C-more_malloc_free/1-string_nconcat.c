54316d18957949c69f5f3491db19ad0e19fd3399
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-06 01:39:18 -0400

    msg

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*string_nconcat - concatinate 2 string till n size
+*@s1: destination to cat
+*@s2: source to cat 
+*@n:size to cat 
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    char *concat;
+    concat = malloc(sizeof(s2) * n);
+    int x = 0;
+
+    while(s1[x])
+    {
+        concat[x]=s1[x];
+        x++;
+    }
+    int y = 0;
+    while(y < n)
+    {
+        concat[x]=s2[y];
+        x++;
+        y++;
+    }
+    concat[x] = '\0';
+    
+    return(concat);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef __MAIN__
+# define __MAIN__
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+#endif
