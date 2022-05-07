2e894669fc741bd1fdaeb5f32f35ca0777399758
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-07 10:58:20 -0400

    msg

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,46 +1,54 @@
 #include <stdio.h>
 #include <string.h>
-#include "main.h"
-int main(int argc,char **argv)
+#include <stdlib.h>
+/**
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+ */
+
+int main(int argc, char **argv)
 {
-    int a[1000],b[1000];
-    int ans[2000]={0};
-    int i,j,tmp;
-    // char s1[101],s2[101];
-    // scanf(" %s",s1);
-    // scanf(" %s",s2);
-    int l1 = strlen(argv[1]);
-    int l2 = strlen(argv[2]);
-    for(i = l1-1,j=0;i>=0;i--,j++)
-    {
-        a[j] = argv[1][i]-'0';
-    }
-    for(i = l2-1,j=0;i>=0;i--,j++)
-    {
-        b[j] = argv[2][i]-'0';
-    }
-    for(i = 0;i < l2;i++)
-    {
-        for(j = 0;j < l1;j++)
-        {
-            ans[i+j] += b[i]*a[j];
-        }
-    }
-    for(i = 0;i < l1+l2;i++)
-    {
-        tmp = ans[i]/10;
-        ans[i] = ans[i]%10;
-        ans[i+1] = ans[i+1] + tmp;
-    }
-    for(i = l1+l2; i>= 0;i--)
-    {
-        if(ans[i] > 0)
-            break;
-    }
-    for(;i >= 0;i--)
-    {
-        printf("%d",ans[i]);
-    }
-	printf("\n");
-    return 0;
+int a[1000], b[1000],ans[2000]= {0};
+int l1, l2, i, j, tmp;
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+l1 = strlen(argv[1]);
+l2 = strlen(argv[2]);
+for (i = l1 - 1, j = 0; i >= 0; i--, j++)
+{
+a[j] = argv[1][i] - '0';
+}
+for (i = l2 - 1, j = 0; i >= 0; i--, j++)
+{
+b[j] = argv[2][i] - '0';
+}
+for (i = 0; i < l2; i++)
+{
+for (j = 0; j < l1; j++)
+{
+ans[i + j] += b[i] * a[j];
+}
+}
+for (i = 0; i < l1 + l2; i++)
+{
+tmp = ans[i] / 10;
+ans[i] = ans[i] % 10;
+ans[i + 1] = ans[i + 1] + tmp;
+}
+for (i = l1 + l2; i >= 0; i--)
+{
+if (ans[i] > 0)
+break;
+}
+for (; i >= 0; i--)
+{
+printf("%d", ans[i]);
+}
+printf("\n");
+return (0);
 }
