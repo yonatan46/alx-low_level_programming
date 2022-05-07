35f9de7e0708f74ea951fbea6fcbac8d708eb5c4
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-07 10:26:07 -0400

    msg

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,6 +11,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void errors(void);
 int _strlen(char *s);
-int is_digit(char *s);
+int is_digit(char );
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
