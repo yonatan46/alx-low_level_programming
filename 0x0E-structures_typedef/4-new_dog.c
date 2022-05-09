6ac17cbea38c752c0e5ca83501221fa34ed0b5bd
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-09 18:38:55 +0300

    msg

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,53 +1,50 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
-*new_dog - struct returning
-*@name: dog name
-*@age: age of dog
-*@owner: owner name
-*Return: dog struct
-*/
+ * new_dog - new dog
+ * @name: name's dog
+ * @age: age's dog
+ * @owner: owner's dog
+ * Return: newdog
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *dog1;
-int nameSize = 0, ownerSize = 0, loop;
 
-while (name[nameSize] != '\0')
-{
-nameSize++;
-}
-while (owner[ownerSize] != '\0')
-{
-ownerSize++;
-}
-dog1 = malloc(sizeof(dog_t));
-if (dog1 == NULL)
-{
-free(dog1);
-return (NULL);
-}
-dog1->name = malloc(sizeof(dog1->name) * nameSize);
-if (dog1->name == NULL)
-{
-free(dog1->name);
-free(dog1);
-return (NULL);
-}
-for (loop = 0; loop <= nameSize; loop++)
-{
-dog1->name[loop] = name[loop];
-}
-dog1->age = age;
-dog1->owner = malloc(sizeof(dog1->owner) * ownerSize);
-if (dog1->owner == NULL)
-{
-free(dog1->owner);
-free(dog1->name);
-free(dog1);
-return (NULL);
-}
-for (loop = 0; loop <= ownerSize; loop++)
-dog1->owner[loop] = owner[loop];
-return (dog1);
+	int i = 0, j = 0, k;
+	dog_t *doge;
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	doge = malloc(sizeof(dog_t));
+	if (doge == NULL)
+	{
+		free(doge);
+		return (NULL);
+	}
+	doge->name = malloc(i * sizeof(doge->name));
+	if (doge->name == NULL)
+	{
+		free(doge->name);
+		free(doge);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		doge->name[k] = name[k];
+	doge->age = age;
+	doge->owner = malloc(j * sizeof(doge->owner));
+	if (doge->owner == NULL)
+	{
+		free(doge->owner);
+		free(doge->name);
+		free(doge);
+		return (NULL);
+	}
+	for (k = 0; k <= j; k++)
+		doge->owner[k] = owner[k];
+	return (doge);
 }
