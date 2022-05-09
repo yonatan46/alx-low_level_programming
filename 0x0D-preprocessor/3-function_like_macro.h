5c0df5526592b7d281323dd620997c6d4b06a8e1
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-09 02:33:57 -0400

    msg

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef __MACRO__
+# define __MACRO__
+
+# define ABS(X) ((X) < 0 ? -1 * (X) : (X))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef __MACRO__
+# define __MACRO__
+
+# define SUM(x, y) ((x) + (y))
+
+#endif
