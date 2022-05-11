8c93a0c767f9a39aae8ddee3e424fd8bb7261bf5
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-11 18:40:43 +0300

    msg

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,14 +5,14 @@
 *@array: array
 *@size: size of the array
 *@action: a void function
-*Return : void
+*Return: void
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int x = 0;
+int x;
 if (array && size && action)
 {
-while (x < size)
+for(x = 0;x < size; x++)
 {
 (*action)(array[x]);
 x++;
