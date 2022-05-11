eddb4080a651eb3585444236cdafa944f12fba5a
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-11 18:59:35 +0300

    msg

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+*int_index - return index
+*@array: array
+*@cmp: function to run
+*@size: size of array
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+unsigned int x;
+if(size <= 0)
+return(-1);
+if(array && size && cmp)
+{
+for(x = 0; x < size; x++)
+{
+if((*cmp)(array[x]) != 0)
+{
+return(x);
+}
+}
+}
+return(-1);
+}
