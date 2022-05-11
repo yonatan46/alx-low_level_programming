8aea7c114d0c068cad2fe02c69e773a888407854
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-11 18:24:08 +0300

    msg

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* print_name - prints name
+* @name: name input
+* @f: function name
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+{
+f(name);
+}
+}
