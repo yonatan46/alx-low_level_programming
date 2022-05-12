08b6ada138be08587f863fe1fdaf3614e32dd9af
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-12 13:43:55 +0300

    msg

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,11 @@
 #ifndef __VARIADIC__
 #define __VARIADIC__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
+int _putchar(char c);
 
 #endif
