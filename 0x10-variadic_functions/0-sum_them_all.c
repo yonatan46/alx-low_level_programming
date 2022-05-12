381e693c7040f194acbf9d070f0a9f07e29b5820
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-12 13:34:51 +0300

    msg

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include "variadic_functions.h"
 /**
 *sum_them_all - add all va args
 *@n: amount of elemets
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef __VARIADIC__
+#define __VARIADIC__
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
