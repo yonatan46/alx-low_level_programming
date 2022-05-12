9de1d2cabbafd68142b9bd906f0997a711244b9b
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-12 13:32:43 +0300

    msg

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+*sum_them_all - add all va args
+*@n: amount of elemets
+*Return: the sum of the va args
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int x, sum;
+
+	sum = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(args, n);
+
+	for (int i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+		sum += x;
+	}
+}
