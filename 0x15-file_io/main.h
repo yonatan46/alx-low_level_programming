7b947289904852cc020d8e2adabb9f92dffb65b8
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-30 16:08:51 +0300

    msg

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content);
 
 #endif
