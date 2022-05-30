c7e0757befaa21b9e52738777639f207aca15fa3
yonatan46 <yonatankefyalew65@gmail.com>
2022-05-30 15:51:31 +0300

    msg

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
  * read_textfile - reads from file and print it
  * @filename: the name of file
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,13 @@
 #ifndef __main__
 #define __main__
 
+#include <elf.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
