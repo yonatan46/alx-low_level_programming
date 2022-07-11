9567d08ad0533014d3c102cb70e92058d9e98f4c
Edilefg <edlamfassil7878@gmail.com>
2022-07-11 21:34:26 +0300

    sg

--- 0x18-dynamic_libraries/4_islower.c
+++ 0x18-dynamic_libraries/4_islower.c
@@ -1,2 +1,13 @@
 #include "main.h"
-int _islower(int c);
+/**
+*_islower - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+
+return (0);
+}
